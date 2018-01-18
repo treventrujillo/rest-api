c31c8bf336b30fc0539b103fcfebd5a299b80824
Treven Trujillo <treventrujillo@gmail.com>
2018-01-17 17:59:24 -0700

    example request

--- app/controllers/api/requests_controller.rb
+++ app/controllers/api/requests_controller.rb
@@ -1,2 +1,18 @@
 class Api::RequestsController < ApplicationController
+
+  def example_request
+    RestClient.get('http://example.com') {|response, request, result, &block| 
+      case response.code
+        when 200
+          puts "Request Successful!"
+          response
+        when 500
+          puts "Internal Server Error!"
+          response
+        else
+          response.return!(&block)
+      end
+    }
+  end
+
 end
